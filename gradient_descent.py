e80764b24e07b991d28a3ddd0f8ab0ef2be52581
quadraticmuffin <55665157+quadraticmuffin@users.noreply.github.com>
2020-11-09 22:10:27 -0600

    Modified dA_dK to preserve symmetry in gradient

--- gradient_descent.py
+++ gradient_descent.py
@@ -6,7 +6,21 @@ import json
 import warnings
 warnings.filterwarnings("error")
 
-def forward_pass(rate_matrix, pattern, output_rates):
+def choose(a, b):
+    """
+    A short helper function to calculate binomial coefficients.
+    """
+    assert int(a) + int(b) == a + b, f'Choose only takes ints, not {a} and {b}'
+    assert a >= 0 and b >= 0, f'Choose only takes nonnegative ints, not {a} and {b}'
+    if a < b:
+        return 0
+    prod = 1
+    faster_b = min(b, a-b)
+    for i in range(faster_b):
+        prod *= (a-i) / (i+1)
+    return round(prod)
+
+def _forward_pass(rate_matrix, pattern, output_rates):
     """
     Performs a 'forward pass' by analytically finding the steady-state probability
     that each node is excited.
@@ -55,8 +69,8 @@ def gradient(pat, pred, Ainv, output_rates, verbose=False):
             Should be a nxn square matrix. 
         output_rates (np.array): The intrinsic output rate constants assigned to each node.
             Should be a dx1 column vector.
-        verbose (bool): If True, returns all quantities computed, in a dict with keys:
-            Ainv, pred, dNLL_dpred, dpred_dAinv, dAinv_dA, dA_dK, dNLL_dAinv, dNLL_dA, dNLL_dK
+        verbose (bool): If True, returns  a dict with keys:
+            Ainv, pred, dNLL_dpred, dpred_dAinv, dAinv_dA, dA_dK, dNLL_dK
 
     Returns: 
         A dict of all the quantities computed, if verbose is True.
@@ -75,23 +89,31 @@ def gradient(pat, pred, Ainv, output_rates, verbose=False):
     
     dAinv_dA = -np.kron(Ainv, Ainv) # d^2 x d^2
 
-    dA_dK = -np.identity(num_nodes**2) # d^2 x d^2
-    for i,j in it.product(range(num_nodes), range(num_nodes)):
-        dA_dK[(num_nodes+1)*i, num_nodes*j + i] += 1
-    try:
-        dNLL_dAinv = dNLL_dpred @ dpred_dAinv
-    except:
-        print(dNLL_dpred, dpred_dAinv)
-        return 
-    dNLL_dA = dNLL_dAinv @ dAinv_dA
-    dNLL_dK = dNLL_dA @ dA_dK
-    dNLL_dK = dNLL_dK.reshape((num_nodes, num_nodes), order='F') # 1 x d^2 -> d x d
+    dA_dK = np.zeros((num_nodes**2, choose(num_nodes, 2))) # d^2 x (d choose 2)
+    idx = 0
+    for i,j in it.combinations(range(num_nodes), 2):
+        dA_dK[i*num_nodes + i, idx] = 1
+        dA_dK[j*num_nodes + j, idx] = 1
+        dA_dK[i*num_nodes + j, idx] = -1
+        dA_dK[j*num_nodes + i, idx] = -1
+        idx += 1
+
+    dNLL_dK = (dNLL_dpred @ dpred_dAinv @ dAinv_dA @ dA_dK).flatten()
+
+    reshaped_dNLL_dK = np.zeros((num_nodes, num_nodes))
+    idx = 0
+    for i,j in it.combinations(range(num_nodes), 2):
+        reshaped_dNLL_dK[i,j] = dNLL_dK[idx]
+        idx += 1
+    reshaped_dNLL_dK = reshaped_dNLL_dK + reshaped_dNLL_dK.T
+    # 1 x (d choose 2) -> d x d, duplicating items for symmetry
+    # and making diagonal terms 0
 
     if verbose:
-        return {'Ainv':Ainv, 'pred':pred, 'dNLL_dpred':dNLL_dpred, 'dpred_dAinv':dpred_dAinv, 'dAinv_dA':dAinv_dA, 'dA_dK':dA_dK, 
-        'dNLL_dAinv':dNLL_dAinv, 'dNLL_dA':dNLL_dA, 'dNLL_dK':dNLL_dK}
+        return {'Ainv':Ainv, 'pred':pred, 'dNLL_dpred':dNLL_dpred, 'dpred_dAinv':dpred_dAinv, 
+            'dAinv_dA':dAinv_dA, 'dA_dK':dA_dK, 'reshaped_dNLL_dK':reshaped_dNLL_dK}
     else:
-        return dNLL_dK
+        return reshaped_dNLL_dK
     
 def nll(pat, pred):
     """
@@ -105,7 +127,7 @@ def nll(pat, pred):
 
 def train(train_data, output_rates, step_size, iters, report_every=0):
     """
-    Trains a new network on given training patterns.
+    Trains a new network on given training patterns using batch gradient descent.
 
     Args:
         train_data (np.array): 2d array where each column is a training pattern.
@@ -125,6 +147,7 @@ def train(train_data, output_rates, step_size, iters, report_every=0):
     """
     d, n = train_data.shape
     K = np.random.rand(d, d) / 10 + 0.45  # weights initialized at 0.5 +/- 0.05
+    # TODO better starting rate parameters?
     K = (K + K.T) / 2  # ensure weights are symmetric
     np.fill_diagonal(K, 0)
     err_over_time = []
@@ -134,20 +157,18 @@ def train(train_data, output_rates, step_size, iters, report_every=0):
         dK = 0
         for j in range(n):  
             train_pattern = train_data[:, j:j+1]
-            Ainv, pred = forward_pass(K, train_pattern, output_rates)
+            Ainv, pred = _forward_pass(K, train_pattern, output_rates)
             dNLL_dK = gradient(train_pattern, pred, Ainv, output_rates)
 
             dK += dNLL_dK
             avg_err += nll(train_pattern, pred)/n
 
-
         K -= step_size * dK
-        # K = (K + K.T)/2 # TODO this seems wrong
         K[K<0] = 0 # NOTE ReLU; ensures all weights are positive.
         err_over_time.append(avg_err)
     
-        if report_every and not i % report_every:
-            print(f'Rates on iteration {i}: \n{K}')
+        if not (i-1) % report_every:
+            print(f'Rates on iteration {i-1}: \n{K}')
     return K, err_over_time
     
 def test(rate_matrix, test_data, output_rates):
@@ -166,27 +187,29 @@ def test(rate_matrix, test_data, output_rates):
     total_err = 0
     for i in range(n):
         test_pattern = test_data[:, i:i+1]
-        pred = forward_pass(rate_matrix, test_pattern, output_rates)[1]
+        pred = _forward_pass(rate_matrix, test_pattern, output_rates)[1]
         total_err += nll(test_pattern, pred)
     avg_err = total_err/n
     return avg_err
 
 if __name__ == '__main__':
-    num_nodes = 5
+    num_nodes = 10
     num_patterns = 5
     train_data = np.random.randint(2, size = (num_nodes, num_patterns))
-    iters = 10000
-    step_size = 0.002
+    # train_data = np.array([[0, 1], [0, 1], [0, 1], [1, 0], [1, 0]])
+    iters = 2000
+    step_size = 0.001
     outs = np.full(num_nodes, 0.5)
-    K, err_over_time = train(train_data, outs, step_size, iters, report_every=1000)
+    K, err_over_time = train(train_data, outs, step_size, iters, report_every=iters/10)
     print (K)
     print (train_data)
     plt.plot(np.arange(iters), err_over_time)
-    plt.ion()
+    # plt.ion()
     plt.show()
 
 #TODO add regularization
-#TODO why does K become asymmetric? use lagrange multipliers
+#TODO least squares implementation 
+#TODO why does K become asymmetric? use lagrange multipliers?
 #TODO train on off by 1
 #TODO weights tend to 0. why? do other hopfield networks/RBMs run into the same problem?
-#TODO properties of diagonally dominant matrices
+#TODO adagrad? other step size optimizer?
--- test_gradient_descent.py
+++ test_gradient_descent.py
@@ -7,36 +7,34 @@ def test_forward_pass():
     rates = np.array([[0., 1],[1, 0]])
     inps = np.array([[1.,1]]).T
     outs = np.full((2, 1), 0.5)
-    pred = gd.forward_pass(rates, inps, outs)[1]
+    pred = gd._forward_pass(rates, inps, outs)[1]
     assert np.allclose(pred, np.array([[2/3, 2/3]]).T)
 
 def test_gradient():
     rates = np.array([[0., 1],[1, 0]])
     inps = np.array([[1.,0]]).T
     outs = np.full((2, 1), 0.5)
-    # pat = np.array([[1., 0]])
-    Ainv, pred = gd.forward_pass(rates, inps, outs)
+    
+    Ainv, pred = gd._forward_pass(rates, inps, outs)
     grads = gd.gradient(inps, pred, Ainv, outs, verbose=True)
     expected = {
         'Ainv': np.array([[0.54545455, 0.36363636],
             [0.36363636, 0.90909091]]), 
         'pred': np.array([[0.54545455],
-            [0.36363636]]), 
+            [0.36363636]]),
         'dNLL_dpred': np.array([[-1.83333333,  1.57142857]]), 
         'dpred_dAinv': np.array([[1., 0., 0., 0.],
             [0., 1., 0., 0.]]), 
-        'dAinv_dA': np.array([[0.29752066, 0.19834711, 0.19834711, 0.1322314 ],
+        'dAinv_dA': -np.array([[0.29752066, 0.19834711, 0.19834711, 0.1322314 ],
             [0.19834711, 0.49586777, 0.1322314 , 0.33057851],
             [0.19834711, 0.1322314 , 0.49586777, 0.33057851],
             [0.1322314 , 0.33057851, 0.33057851, 0.82644628]]), 
-        'dA_dK': np.array([[ 0., -0.,  1., -0.],
-            [-0., -1., -0., -0.],
-            [-0., -0., -1., -0.],
-            [-0.,  1., -0.,  0.]]), 
-        'dNLL_dAinv': np.array([[-1.83333333,  1.57142857,  0.        ,  0.        ]]), 
-        'dNLL_dA': np.array([[-0.23376623,  0.41558442, -0.15584416,  0.27705628]]), 
-        'dNLL_dK': np.array([[ 0.        , -0.07792208],
-            [-0.13852814,  0.        ]])}
+        'dA_dK': np.array([[1.],
+            [-1.],
+            [-1.],
+            [1.]]), 
+        'reshaped_dNLL_dK': np.array([[ 0.        , 0.21645022],
+            [0.21645022,  0.        ]])}
     for k in expected:
         assert np.allclose(grads[k], expected[k])
 
