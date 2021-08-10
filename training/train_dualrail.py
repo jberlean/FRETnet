b14cf2c11f10489c87cb9cb01e88643f0746148b
Joseph Berleant <jberleant@gmail.com>
2021-08-10 14:23:16 -0400

    minor changes to train_dualrail output

--- training/train_dualrail.py
+++ training/train_dualrail.py
@@ -16,6 +16,8 @@ from objects import utils as objects
 
 from train_utils import off_patterns, Ainv_from_rates, A_from_rates, k_in_from_input_data, network_from_rates, rate_from_positions, random_point_on_sphere
 
+np.set_printoptions(precision=2)
+
 # TODO: use functions in train_singlerail.py rather than reimplementing them here
 
 ########################
